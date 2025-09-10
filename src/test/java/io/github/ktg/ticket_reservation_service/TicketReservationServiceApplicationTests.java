84462eecf7750b4b5ba5f7552563869cd0ab2a07
KTG5672 <zzang5992@gmail.com>
2025-09-10 21:56:38 +0900

    Initial commit

---
+++ src/test/java/io/github/ktg/ticket_reservation_service/TicketReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ktg.ticket_reservation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
