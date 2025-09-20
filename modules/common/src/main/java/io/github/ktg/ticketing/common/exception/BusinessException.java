67e337c265250402d2d08849958c298ee2b00a9c
KTG5672 <zzang5992@gmail.com>
2025-09-20 18:52:15 +0900

    refactor: 공통 에러 처리 코드 주석 추가 - 클래스 설명 및 규칙 작성

--- modules/common/src/main/java/io/github/ktg/ticketing/common/exception/BusinessException.java
+++ modules/common/src/main/java/io/github/ktg/ticketing/common/exception/BusinessException.java
@@ -2,6 +2,11 @@ package io.github.ktg.ticketing.common.exception;
 
 import lombok.Getter;
 
+/**
+ * 비지니스 예외 최상위 클래스
+ * - 모든 비지니스 예외는 해당 클래스를 상속
+ * - ErrorCode 인터페이스를 가짐
+ */
 @Getter
 public class BusinessException extends RuntimeException{
 
--- modules/common/src/main/java/io/github/ktg/ticketing/common/exception/CommonErrorCode.java
+++ modules/common/src/main/java/io/github/ktg/ticketing/common/exception/CommonErrorCode.java
@@ -2,6 +2,10 @@ package io.github.ktg.ticketing.common.exception;
 
 import lombok.AllArgsConstructor;
 
+/**
+ * 공통 ErrorCode 클래스
+ * - 도메인 및 비지니스 규칙과 무관한 ErrorCode
+ */
 @AllArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
 
