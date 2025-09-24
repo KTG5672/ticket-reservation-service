44e4a5400eb4ceb3f789bd7023434be4892ca7bc
KTG5672 <zzang5992@gmail.com>
2025-09-24 23:36:47 +0900

    feat: BaseEntity 작성 및 JPA Auditing 기능 추가 - Entity 상위 추상 클래스(BaseEntity) 추가 - 현재 사용자 식별자 제공 인터페이스 (CurrentUserProvider) 정의 - JpaAuditingConfig 추가 (AuditorAware, 기본 사용자 system)

---
+++ modules/persistence/src/main/java/io/github/ktg/ticketing/entity/config/JpaAuditingConfig.java
@@ -0,0 +1,40 @@
+package io.github.ktg.ticketing.entity.config;
+
+import io.github.ktg.ticketing.common.security.CurrentUserProvider;
+import java.util.Optional;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * Jpa Auditing Config 클래스
+ * - Auditing Auto Config
+ * - CurrentUserProvider 이용한 작성/수정자 기능 제공
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+    /**
+     * 현재 사용자의 식별자를 @CreatedBy, @LastModifiedBy 필드에 제공
+     * @param currentUserProvider 현재 사용자 정보 제공 인터페이스
+     * @return AuditorAware<String>
+     */
+    @Bean
+    public AuditorAware<String> auditorAware(CurrentUserProvider currentUserProvider) {
+        return () -> Optional.of(currentUserProvider.getCurrentUserId());
+    }
+
+    /**
+     * CurrentUserProvider 구현체가 없을 때 Default Bean 제공
+     * @return CurrentUserProvider
+     */
+    @Bean
+    @ConditionalOnMissingBean(CurrentUserProvider.class)
+    public CurrentUserProvider defaultCurrentUserProvider() {
+        return () -> "system";
+    }
+
+}
