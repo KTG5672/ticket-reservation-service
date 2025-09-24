d70417d5e89636348c2688e8ddf4f25351b88e99
KTG5672 <zzang5992@gmail.com>
2025-09-24 23:50:39 +0900

    test: JPA Auditing 기능 테스트 - 테스트 용도 Entity (TestEntity) 추가 - 테스트 용도 SpringBootConfiguration (PersistenceTestApplication) 추가 - 생성일, 생성자 테스트 (createdAt, createdBy) - 수정일, 수정자 테스트 (updatedAt, updatedBy)

---
+++ modules/persistence/src/test/java/io/github/ktg/ticketing/entity/TestEntity.java
@@ -0,0 +1,32 @@
+package io.github.ktg.ticketing.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 테스트용 Entity
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+public class TestEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public TestEntity(String name) {
+        this.name = name;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
+}
