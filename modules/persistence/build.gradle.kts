717f222db6c4288ea46b050dfb4cb26cc8972444
KTG5672 <zzang5992@gmail.com>
2025-09-24 21:50:12 +0900

    chore: JPA 의존성 추가 - Spring Data JPA 추가

--- modules/persistence/build.gradle.kts
+++ modules/persistence/build.gradle.kts
@@ -1,7 +1,17 @@
 plugins {
     id("java")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
+tasks.named<Jar>("jar") {
+    enabled = true
 }
 dependencies {
     implementation(project(":modules:common"))
     implementation(project(":modules:domain"))
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 }
