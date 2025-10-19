0a5fd80ac265bba2c228b1d316f44e1b1ac93e48
KTG5672 <zzang5992@gmail.com>
2025-10-19 23:57:17 +0900

    feat: spring-docs 의존성 추가 및 openAPI 설정 - spring docs 의존성 추가 (springdoc-openapi-starter-webmvc-ui:2.6.0) - Open API 설정 추가 (OpenApiConfig)

---
+++ modules/app/src/main/java/io/github/ktg/ticketing/app/config/OpenApiConfig.java
@@ -0,0 +1,64 @@
+package io.github.ktg.ticketing.app.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+    info = @Info(title = "Ticket Reservation Service API", version = "v1"),
+    security = {@SecurityRequirement(name = "bearerAuth")}
+)
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().components(
+            new Components().addSecuritySchemes(
+                "bearerAuth",
+                new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")
+            )
+        );
+    }
+
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+            .group("user")
+            .packagesToScan("io.github.ktg.ticketing.app.user")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi reservationApi() {
+        return GroupedOpenApi.builder()
+            .group("reservation")
+            .packagesToScan("io.github.ktg.ticketing.app.reservation")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi eventApi() {
+        return GroupedOpenApi.builder()
+            .group("event")
+            .packagesToScan("io.github.ktg.ticketing.app.event")
+            .build();
+    }
+
+    @Bean
+    public GroupedOpenApi paymentApi() {
+        return GroupedOpenApi.builder()
+            .group("payment")
+            .packagesToScan("io.github.ktg.ticketing.app.payment")
+            .build();
+    }
+}
