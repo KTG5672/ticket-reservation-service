de22177d5cd7c7cc749afbf4de74988c03323b1a
KTG5672 <zzang5992@gmail.com>
2025-10-24 21:53:43 +0900

    feat: 로그인 유스케이스 구현 및 테스트 추가 - 로그인 유스케이스 구현 (SignInService) - 존재하지 않는 이메일, 비밀번호 불일치 예외 처리 - AccessToken 발급 후 반환 추가 (SignInResult) - 관련 테스트 추가

---
+++ modules/app/src/main/java/io/github/ktg/ticketing/app/user/service/SignInService.java
@@ -0,0 +1,51 @@
+package io.github.ktg.ticketing.app.user.service;
+
+import io.github.ktg.ticketing.domain.user.exception.EmailNotValidException;
+import io.github.ktg.ticketing.domain.user.exception.PasswordNotValidException;
+import io.github.ktg.ticketing.domain.user.exception.UserErrorCode;
+import io.github.ktg.ticketing.domain.user.model.User;
+import io.github.ktg.ticketing.domain.user.port.in.SignInCommand;
+import io.github.ktg.ticketing.domain.user.port.in.SignInResult;
+import io.github.ktg.ticketing.domain.user.port.in.SignInUseCase;
+import io.github.ktg.ticketing.domain.user.port.out.PasswordEncoderPort;
+import io.github.ktg.ticketing.domain.user.port.out.TokenProvider;
+import io.github.ktg.ticketing.domain.user.port.out.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * 로그인 서비스
+ * - 로그인 유스케이스 구현 (SignInUseCase)
+ */
+@Service
+@RequiredArgsConstructor
+public class SignInService implements SignInUseCase {
+
+    private final TokenProvider tokenProvider;
+    private final UserRepository userRepository;
+    private final PasswordEncoderPort passwordEncoderPort;
+
+    /**
+     * 로그인
+     * - 존재 하지 않는 이메일 일 때 예외 (EmailNotValidException)
+     * - 패스워드 불일치 시 예외 (PasswordNotValidException)
+     * - 로그인 검증 후 AccessToken, Refresh(예정) 발급
+     * @param command 로그인 정보
+     * @return SignInResult(AccessToken, RefreshToken)
+     */
+    @Override
+    public SignInResult signIn(SignInCommand command) {
+        String email = command.email();
+        String password = command.password();
+
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new EmailNotValidException(UserErrorCode.EMAIL_NOT_FOUND));
+
+        if (!passwordEncoderPort.matches(password, user.getPassword().value())) {
+            throw new PasswordNotValidException(UserErrorCode.PASSWORD_NOT_MATCHED);
+        }
+
+        String accessToken = tokenProvider.generateAccessToken(user.getId());
+        return new SignInResult(accessToken, null);
+    }
+}
