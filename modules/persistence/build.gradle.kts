41d7df9cebc3bb02d9144c187ff6a63cc723f0fa
KTG5672 <zzang5992@gmail.com>
2025-12-03 23:04:17 +0900

    feat: Querydsl 의존성 추가 및 설정 - 의존성 추가 (Querydsl 5.0.0) - QuerydslConfig JPAQueryFactory Bean 등록

--- modules/persistence/build.gradle.kts
+++ modules/persistence/build.gradle.kts
@@ -15,8 +15,13 @@ dependencies {
     implementation(project(":modules:domain"))
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
 
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:junit-jupiter")
---
+++ modules/persistence/src/main/java/io/github/ktg/ticketing/persistence/config/QuerydslConfig.java
@@ -0,0 +1,24 @@
+package io.github.ktg.ticketing.persistence.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * QueryDsl Config 클래스
+ * - 쿼리 생성을 위한 JPAQueryFactory Bean 등록
+ */
+@Configuration
+@RequiredArgsConstructor
+public class QuerydslConfig {
+
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
